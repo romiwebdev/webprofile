65a6e04ff44a6c1658f6cb01d3c727fc03842253
Muhromin <138580927+romiwebdev@users.noreply.github.com>
2024-11-25 21:28:57 +0700

    Update settings.py

--- webprofile/settings.py
+++ webprofile/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$$l@lrpu2)dtma*=+f!f72kmhv3@ol)--^3)^wfggic8e)cfa='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
